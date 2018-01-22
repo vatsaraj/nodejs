bfdbb1fde1c9127d90d5afe80958d7bd5f38d0ec
vatsaraj <vatsaraj@gmail.com>
2018-01-23 02:01:05 +0530

    Added a new handlebar file and some code in server.js to display the HTTP headers sent by clients.

--- practice/p02/server.js
+++ practice/p02/server.js
@@ -38,6 +38,15 @@ app.get('/help', (req, res) => {
   res.type('text/html');
   res.render('help');
 });
+app.get('/headers', (req, res) => {
+  // This page displays the HTTP headers that are sent to the webserver
+  // by a client.
+  res.type('text/html');
+  var headers = '';
+  //for(var name in req.headers) headers.push(name + ' : ' + req.headers[name] + '\n');
+  for(var name in req.headers) headers += name + ' : ' + req.headers[name] + ', ';
+  res.render('headers', {dataBlock: headers});
+});
 // ::END:: Define routes to particular webpages.
 
 // Specify a path which will be looked into, by default, when the browser
